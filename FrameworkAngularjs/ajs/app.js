cb10f06cabbdd91375271e371756334266916dc0
Matt Brewerton <matt.brewerton@clickdatabases.co.uk>
2020-08-14 15:25:07 +0100

    Initial commit

---
+++ FrameworkAngularjs/ajs/app.js
@@ -0,0 +1,13 @@
+﻿angular.module('app', []);
+
+angular.module('app')
+    .directive('myAjsDirective', [
+        function () {
+            return {
+                restrict: 'E',
+                template: `
+                    <h1>myAjsDirective Works!</h1>
+                `
+            }
+        }
+    ])
